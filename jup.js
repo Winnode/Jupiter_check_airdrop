d08a418bdf2c94076780171d31d55c74434ded35
WinNode <117538854+Winnode@users.noreply.github.com>
2023-12-02 13:40:26 +0700

    Update jup.js

--- jup.js
+++ jup.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
   try {
